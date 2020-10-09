267342b0d76d6bc0b7da500c056bf5063177eedf
kzhang0523 <70768441+kzhang0523@users.noreply.github.com>
2020-10-09 17:50:22 -0400

    Create Timer

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text highScore;
+    public Text timeScore;
+    public bool timerActive = true;
+    public float timeTaken;
+    public float sceneBestTime = 60f;
+    private void Start()
+    {
+        highScore.text = PlayerPrefs.GetFloat("HighScore", 0).ToString();
+        sceneBestTime = PlayerPrefs.GetFloat("CurrentBestTime", sceneBestTime);
+    }
+    private void Update()
+    {
+        if (timerActive)
+        {
+            timeTaken += Time.deltaTime;
+            timeScore.text = timeTaken.ToString();
+        }
+       if(Goal.goalMet == true){
+            StopTimer();
+        }
+    }
+    public void StopTimer()
+    {
+        timerActive = false;
+        if (timeTaken < sceneBestTime)
+        {
+            highScore.text = timeTaken.ToString();
+            PlayerPrefs.SetFloat("CurrentBestTime", timeTaken);
+            PlayerPrefs.SetFloat("HighScore", timeTaken);
+        }
+    }
+}
