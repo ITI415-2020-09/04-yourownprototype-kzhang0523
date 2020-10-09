0d296de7a44930fa8226f546dc949d0b6e99f3a8
kzhang0523 <70768441+kzhang0523@users.noreply.github.com>
2020-10-09 16:25:16 -0400

    Add Goal Script

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+
+{
+    static public bool goalMet = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "SoccerBall")
+        {
+            Goal.goalMet = true;
+            Material mat = GetComponent<Renderer>().material;
+            Color c = mat.color;
+            c.a = 1;
+            mat.color = c;
+        }
+    }
+}
